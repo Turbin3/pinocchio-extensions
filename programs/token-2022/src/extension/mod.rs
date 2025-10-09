800b828e177c27f7a8b44a09ea36d4a82764d807
Mike Olson <mike@madtownmining.com>
2025-10-09 12:37:44 -0500

    Move default account state into extension (singular).

--- programs/token-2022/src/extension/mod.rs
+++ programs/token-2022/src/extension/mod.rs
@@ -1,4 +1,5 @@
 pub mod consts;
+pub mod default_account_state;
 pub mod group_member_pointer;
 pub mod group_pointer;
 pub mod permanent_delegate;
--- programs/token-2022/src/extensions/mod.rs
+++
@@ -1,4 +0,0 @@
-//! Extensions available to token mints and accounts
-
-/// Default Account State extension
-pub mod default_account_state;
