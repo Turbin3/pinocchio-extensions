de500c24f9f443e01c5b888754dc97b2addec43d
inspiration_gx <conservedinnovation@gmail.com>
2026-02-10 10:03:55 +0100

    token2022: add non_transferrable extension

--- programs/token-2022/src/instructions/extensions/non_transferrable/initialize.rs
+++ programs/token-2022/src/instructions/extensions/non_transferrable/initialize.rs
@@ -1,12 +1,8 @@
 use {
     crate::instructions::extensions::ExtensionDiscriminator,
-    core::{mem::MaybeUninit, slice},
     solana_account_view::AccountView,
     solana_address::Address,
-    solana_instruction_view::{
-        cpi::{invoke_signed_with_bounds, Signer},
-        InstructionAccount, InstructionView,
-    },
+    solana_instruction_view::{cpi, InstructionAccount, InstructionView},
     solana_program_error::ProgramResult,
 };
 
@@ -32,56 +28,27 @@ pub struct Initialize<'a, 'b> {
 impl Initialize<'_, '_> {
     #[inline(always)]
     pub fn invoke(&self) -> ProgramResult {
-        self.invoke_signed(&[])
-    }
-
-    #[inline(always)]
-    pub fn invoke_signed(&self, signers: &[Signer]) -> ProgramResult {
         let &Self {
             mint,
             token_program,
         } = self;
 
         // Account Metadata
-        const UNINIT_INSTRUCTION_ACCOUNTS: MaybeUninit<InstructionAccount> =
-            MaybeUninit::<InstructionAccount>::uninit();
-        let mut accounts = [UNINIT_INSTRUCTION_ACCOUNTS; 1];
-
-        unsafe {
-            // SAFETY:
-            // - `accounts` is sized to 1
-            // - Index 0 is always present (Mint)
-            accounts
-                .get_unchecked_mut(0)
-                .write(InstructionAccount::writable(mint.address()));
-        }
+        let accounts = [InstructionAccount::writable(mint.address())];
 
         // Instruction Data
         // Unlike MemoTransfer or CpiGuard which toggle state on a Token Account,
         // NonTransferable permanently initializes a Mint.
-        // Therefore, it does not accept a toggle byte (Enable/Disable).
+        // Therefore, it does not accept a toggle byte 0/1 (Enable/Disable)
+        // and does not need an authority since it's permanent
         let data = &[ExtensionDiscriminator::InitializeNonTransferableMint as u8];
 
         let instruction = InstructionView {
             program_id: token_program,
             data,
-            accounts: unsafe { slice::from_raw_parts(accounts.as_ptr() as _, 1) },
+            accounts: &accounts,
         };
 
-        // Account Views for CPI
-        const UNINIT_ACCOUNT_VIEWS: MaybeUninit<&AccountView> = MaybeUninit::uninit();
-        let mut account_views = [UNINIT_ACCOUNT_VIEWS; 1];
-
-        unsafe {
-            // SAFETY:
-            // - `account_views` is sized to 1
-            account_views.get_unchecked_mut(0).write(mint);
-        }
-
-        invoke_signed_with_bounds::<1>(
-            &instruction,
-            unsafe { slice::from_raw_parts(account_views.as_ptr() as *const &AccountView, 1) },
-            signers,
-        )
+        cpi::invoke(&instruction, &[mint])
     }
 }
